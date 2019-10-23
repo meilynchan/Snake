75ddacb19ae911dac952a86117561e0f399fd807
meilynchan <56370194+meilynchan@users.noreply.github.com>
2019-10-23 17:55:27 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,182 +1,52 @@
-// setup canvas
+// set up our canvas to fill the screen
+
 let canvas = document.getElementById('display')
 canvas.width = document.body.clientWidth
 canvas.height = document.body.clientHeight
 let ctx = canvas.getContext('2d')
 
-// variables
-let gridSize = 40
-let gridWidth = Math.floor(canvas.width / gridSize)
-let gridHeight = Math.floor(canvas.height / gridSize)
+// game logic
 
-let snake = [ 
-  {x: 3, y: 1},
-  {x: 3, y: 2}, 
-  {x: 3, y: 3}, 
-  {x: 4, y: 3}, 
-  {x: 5, y: 3},
-]
+let msUntilReady = 2000 + (Math.random() * 3000) // 2-5 seconds of waiting
+let msUntilLate = msUntilReady + 500 // half a second to react
 
-let direction = 'right'
+let start = new Date()
+let ready = new Date(start.getTime() + msUntilReady)
+let late = new Date(start.getTime() + msUntilLate)
 
-let apple = {x: 7, y: 3}
-
-function resetSnake(){
-  snake = [ 
-    {x: 3, y: 1},
-    {x: 3, y: 2}, 
-    {x: 3, y: 3}, 
-    {x: 4, y: 3}, 
-    {x: 5, y: 3},
-
-
-]
-
-direction = 'right'
-
-
-}
-
-function randomApple(){
-  apple = {
-    x: Math.floor(Math.random() * gridWidth),
-    y: Math.floor(Math.random() * gridHeight),
+window.addEventListener('keydown', event => {
+  console.log(event)
+  if (event.code == 'Enter') {
+    let now = new Date()
+    if (now < ready) {
+      alert('too early!')
+    } else if (now > late) {
+      alert('too slow!')
+    } else {
+      alert('YOU WIN!!!')
+    }
   }
+})
+
+// todo how to make this graphical?
+
+function drawSquare(color){
+  // make disappear
+  
+
+  ctx.fillStyle = color
+  ctx.fillRect(canvas.width/2 - 250, canvas.height/2 - 250, 50, 0)
 
 }
 
-// draw helpers
-function erase() {
-  ctx.fillStyle = '#000044'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-}
-function drawSquare(x, y) {
-  ctx.fillStyle = 'green'
-  ctx.fillRect(x * gridSize, y * gridSize, gridSize, gridSize)
-}
-function drawHead(x, y) {
-  ctx.fillStyle = 'darkgreen'
-  ctx.fillRect(x * gridSize, y * gridSize, gridSize, gridSize)
-}
-function drawCircle(x, y) {
+function drawCircle(){
   ctx.fillStyle = 'red'
   ctx.beginPath()
-  ctx.arc((x + 0.5) * gridSize, (y + 0.5) * gridSize, gridSize / 2, 0, 2 * Math.PI)
+  ctx.arc(canvas.width/2 - 250, canvas.height/2 - 250, canvas.height/4, 0, 0, Math.PI * 3, false)
   ctx.fill()
 }
 
-function drawSnake() {
-  for(let i = 0; i < snake.length - 1; i++){
-    let point = snake[i]
-    drawSquare(point.x, point.y)
-  }
-  let head = snake[snake.length - 1]
-  drawHead(head.x, head.y)
-}
 
 
-// user input
-window.addEventListener('keydown', event => {
-  console.log(event.code)
-
-if(event.code === 'ArrowDown'){
-  direction = 'down'
-}
-
-if(event.code === 'ArrowUp'){
-  direction = 'up'
-}
-
-if(event.code === 'ArrowRight'){
-  direction = 'right'
-}
-
-if(event.code === 'ArrowLeft'){
-  direction = 'left'
-}
-
-
-})
-
-// todo program the game
-
-
-function loop(){
-  console.log(snake)
-
-  let head = snake[snake.length - 1]
-
-if(direction === 'right'){
-  let newHead = {x: head.x + 1, y: head.y}
-  snake.push(newHead)
-  }
-
-  if(direction === 'left'){
-  let newHead = {x: head.x - 1, y: head.y}
-  snake.push(newHead)
-  }
-
-if(direction === 'down'){
-  let newHead = {x: head.x, y: head.y + 1}
-  snake.push(newHead)
-  }
-
-if(direction === 'up'){
-  let newHead = {x: head.x, y: head.y - 1}
-  snake.push(newHead)
-  }
-
-  head = snake[snake.length - 1]
-  if(head.x === apple.x && head.y === apple.y){
-    console.log('ate the apple')
-    
-    randomApple()
-
-  } else{
-
-    snake.shift()
-
-  }
-
-
-  if(head.x >= gridWidth || head.y >= gridHeight || head.y < 0 || head.x < 0){
-
-    resetSnake()
-
-  }
-
-
- 
-  for(let i = 0; i < snake.length - 1; i++){
-    let point = snake[i]
-
-    if(head.x === point.x && head.y === point.y){
-
-      resetSnake()
-    }
-
-  }
-
-
-
-
-  erase()
-  drawSnake()
-  drawCircle(apple.x, apple.y)
-
-  setTimeout(() => loop(), 100)
-}
-
-loop()
-
-
-
-
-
-
-
-
-
-
-
-
+setTimeout(() => drawCircle(), msUntilReady)
+setTimeout(() => drawSquare('red'), msUntilLate)
